1840591f1ce89394a10b4c069b0f6f1c098b451f
98jmilton <98jmilton@gmail.com>
2021-08-31 14:54:14 +0100

    Creation of webpage Header and main page "Article List" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+
+const routes: Routes = [
+  {path: '', component: ArticleListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
